c1fab518a5406378959f1fff418ba0f56f744e84
nick repasy <nrepasy@gmail.com>
2020-11-30 21:02:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
  
  name_hash.each do |name, num|
-  if num <= num[0,1,2]
+  if num <= num[0]
     puts #{name}
 end
 end
