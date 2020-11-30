81b4b66b07e2bb3bcedb72679014b67f054ea90d
nick repasy <nrepasy@gmail.com>
2020-11-30 20:44:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ hash = {:blake => 500, :ashley => 2, :adam => 1}
  name_hash.each do |name, num|
-  if num 
+  if num[0] < [num][1] 
    lowest = #{name}
   end
 
